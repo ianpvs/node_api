b34abd79ddac4c492a2d6dd6290eb27f9de5e9d4
ianpvs <ianviana_2009@hotmail.com>
2021-10-28 01:38:36 -0300

    chore: config lint and husty

--- src/presentation/controller/Signup.ts
+++ src/presentation/controller/Signup.ts
@@ -1,8 +1,9 @@
 export default class SignUpController {
-    
+
     handle = (httpRequest: any): any => {
         return {
-            statusCode: 400
+            statusCode: 400,
+            body: new Error('Missing param: name')
         };
     }
 }
